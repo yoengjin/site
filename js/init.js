0b0b4731374c19db0c2578611506044ce39e6b30
SunghoLee <eshaj@kaist.ac.kr>
2019-03-23 15:14:48 +0900

    Change theme

--- js/init.js
+++ js/init.js
@@ -1,57 +1,124 @@
+---
+---
 /*
-	Linear by TEMPLATED
-    templated.co @templatedco
-    Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+	Ion by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
 */
 
-skel.init({
-	prefix: 'css/style',
-	resetCSS: true,
-	boxModel: 'border',
-	grid: {
-		gutters: 50
-	},
-	breakpoints: {
-		'mobile': {
-			range: '-480',
-			lockViewport: true,
-			containers: 'fluid',
-			grid: {
-				collapse: true,
-				gutters: 10
+(function($) {
+
+	skel.init({
+		reset: 'full',
+		breakpoints: {
+			
+			// Global.
+				global: {
+					range: '*',
+					href: '{{ site.baseurl }}/css/style.css',
+					containers: 1400,
+					grid: {
+						gutters: {
+							vertical: '4em',
+							horizontal: 0
+						}
+					}
+				},
+
+			// XLarge.
+				xlarge: {
+					range: '-1680',
+					href: '{{ site.baseurl }}/css/style-xlarge.css',
+					containers: 1200
+				},
+
+			// Large.
+				large: {
+					range: '-1280',
+					href: '{{ site.baseurl }}/css/style-large.css',
+					containers: 960,
+					grid: {
+						gutters: {
+							vertical: '2.5em'
+						}
+					},
+					viewport: {
+						scalable: false
+					}
+				},
+
+			// Medium.
+				medium: {
+					range: '-980',
+					href: '{{ site.baseurl }}/css/style-medium.css',
+					containers: '90%',
+					grid: {
+						collapse: 1
+					}
+				},
+
+			// Small.
+				small: {
+					range: '-736',
+					href: '{{ site.baseurl }}/css/style-small.css',
+					containers: '90%',
+					grid: {
+						gutters: {
+							vertical: '1.25em'
+						}
+					}
+				},
+
+			// XSmall.
+				xsmall: {
+					range: '-480',
+					href: '{{ site.baseurl }}/css/style-xsmall.css',
+					grid: {
+						collapse: 2
+					}
+				}
+
+		},
+		plugins: {
+			layers: {
+				
+				// Config.
+					config: {
+						transform: true
+					},
+				
+				// Navigation Panel.
+					navPanel: {
+						animation: 'pushX',
+						breakpoints: 'medium',
+						clickToHide: true,
+						height: '100%',
+						hidden: true,
+						html: '<div data-action="moveElement" data-args="nav"></div>',
+						orientation: 'vertical',
+						position: 'top-left',
+						side: 'left',
+						width: 250
+					},
+
+				// Navigation Button.
+					navButton: {
+						breakpoints: 'medium',
+						height: '4em',
+						html: '<span class="toggle" data-action="toggleLayer" data-args="navPanel"></span>',
+						position: 'top-left',
+						side: 'top',
+						width: '6em'
+					}
+
 			}
-		},
-		'desktop': {
-			range: '481-',
-			containers: 1200
-		},
-		'1000px': {
-			range: '481-1200',
-			containers: 960
 		}
-	}
-}, {
-	panels: {
-		panels: {
-			navPanel: {
-				breakpoints: 'mobile',
-				position: 'left',
-				style: 'reveal',
-				size: '80%',
-				html: '<div data-action="navList" data-args="nav"></div>'
-			}
-		},
-		overlays: {
-			titleBar: {
-				breakpoints: 'mobile',
-				position: 'top-left',
-				height: 44,
-				width: '100%',
-				html: '<span class="toggle" data-action="togglePanel" data-args="navPanel"></span>' +
- '<span class="title" data-action="copyHTML" data-args="logo"></span>'
-			}
-		}
-	}
-	
-	
-});
+	});
+
+	$(function() {
+		
+		// jQuery ready stuff.
+		
+	});
+
+})(jQuery);
